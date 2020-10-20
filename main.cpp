85e81e52ae702ea124c9aaf05844c7152eac6328
jorge montaña <jorge.montana@udea.edu.co>
2020-10-20 11:53:14 -0500

    implemetacion de una nueva funcion

--- main.cpp
+++ main.cpp
@@ -11,15 +11,35 @@ int ejex(int angulo , int Vo, int t);
 int ejey(int angulo, int Vy, int t);
 int main()
 {
-float g=9.8, pi=3.1416;
-int Ho=-100, d=600,Xo=0,Yo=Ho,Do=0.05*d;
-int Hd=-100,xD=d,yD=Hd,dD=0.05*d;
-float aO=50*pi/180, aD=0;
-int i=1, j=0;
+    float g=9.8, pi=3.1416;
+    int Ho,A;
+    cout<<"ingrese la altura y el angulo del disparo ofensivo: ";
+    cin>>Ho>>A;
+    int d=600,Xo=0,Yo=Ho,Do=0.05*d;
+    int Hd=-100,xD=d,yD=Hd,dD=0.05*d;
+    float aO=A*pi/180, aD=0;
+    int iter=1, flag=0;
 
-    cout << "Hello World!" << endl;
-    return 0;
+    for(int VoO=0;VoO<100;VoO+=5){
+        float   VxO=VoO*cos(aO);
+        float   VyO=VoO*sin(aO);
+        for (int t=1;t<1000000;t++){
+            float x=Xo+ VxO*t;
+            float y=Yo+VyO*t-(1/2*g*(t*t));
+            cout<<"x: "<<x<<" y: "<<y<<endl;
+            if(sqrt(pow(d-Hd,2)+pow(x-y,2))<=Do);
+                   flag=1;
+                   break;
+        }
+        iter+=1;
+
+    }
+
+
 }
+
+
+/*
 int random (int n, int desde, int hasta){//Funcion para generar varibles aleatrotias para los parametros de lanzamiento
     int aleatorio;
     for (int i = 1; i <= n; i ++){
@@ -28,24 +48,6 @@ int random (int n, int desde, int hasta){//Funcion para generar varibles aleatro
             }
 }
 
-int ejex(int angulo, int Vo, int t ){ //Funcion para calcular la distancia del proyectil en el eje X
-    int x;
-    int Xo=0;
-    int vx;
-    vx = Vo*cos(angulo);
-    x = Xo+ vx*t;
 
-    return x;
 
-}
-int ejey(int angulo, int Vo, int t ){ //Funcion para calcular la distancia del proyectil en el eje y
-    int Y;
-    float g=9.81;
-    int Yo=0;
-    int vy;
-    vy = Vo*sin(angulo)-g*t;
-    Y = Yo+vy*t-1/2*g*(t*t);
-
-    return Y;
-
-}
+*/
